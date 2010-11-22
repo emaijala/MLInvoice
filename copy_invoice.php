e48ffa04e40a07ee4498293e1b873f2af7a221e2
emaijala <emaijala@893e5d92-dae6-4f76-a1ff-a647e29d1ad7>
2010-11-22 21:05:07 +0000

    Clean up mysql calls, don't copy reminder fee rows.
    git-svn-id: https://vllasku.svn.sourceforge.net/svnroot/vllasku/trunk@19 893e5d92-dae6-4f76-a1ff-a647e29d1ad7

--- copy_invoice.php
+++ copy_invoice.php
@@ -40,8 +40,8 @@ $boolRefund = getRequest('refund', FALSE);
 
 echo htmlPageStart( _PAGE_TITLE_ );
 
-if( $intInvoiceId ) {
-
+if ($intInvoiceId) 
+{
     if ($boolRefund)
     {
       $strQuery = "UPDATE {prefix}invoice " .
@@ -55,71 +55,70 @@ if( $intInvoiceId ) {
         "FROM ". _DB_PREFIX_. "_invoice ".
         "WHERE ". _DB_PREFIX_. "_invoice.id = $intInvoiceId";
     $intRes = mysql_query($strQuery);
-    $intNRows = mysql_numrows($intRes);
-    if( $intNRows ) {
-        
-        $strname = mysql_result($intRes, 0, "name");
-        $intCompanyId = mysql_result($intRes, 0, "company_id");
-        $intInvoiceNo = mysql_result($intRes, 0, "invoice_no");
-        $intRealInvoiceNo = mysql_result($intRes, 0, "real_invoice_no");
-        $intInvoiceDate = mysql_result($intRes, 0, "invoice_date");
-        $intDueDate = mysql_result($intRes, 0, "due_date");
-        $intPaymentDate = mysql_result($intRes, 0, "payment_date");
-        $intRefNumber = mysql_result($intRes, 0, "ref_number");
-        $intStateId = mysql_result($intRes, 0, "state_id");
-        $strReference = mysql_result($intRes, 0, "reference");
-        $intBaseId = mysql_result($intRes, 0, "base_id");
+    if ($row = mysql_fetch_assoc($intRes)) {
+        $strname = $row['name'];
+        $intCompanyId = $row['company_id'];
+        $intInvoiceNo = $row['invoice_no'];
+        $intRealInvoiceNo = $row['real_invoice_no'];
+        $intInvoiceDate = $row['invoice_date'];
+        $intDueDate = $row['due_date'];
+        $intPaymentDate = $row['payment_date'];
+        $intRefNumber = $row['ref_number'];
+        $intStateId = $row['state_id'];
+        $strReference = $row['reference'];
+        $intBaseId = $row['base_id'];
     }
     
     $intDate = date("Ymd");
-    $intDueDate = date("Ymd",mktime(0, 0, 0, date("m"), date("d")+14, date("Y")));
+    $intDueDate = date("Ymd",mktime(0, 0, 0, date("m"), date("d") + $paymentDueDate, date("Y")));
     
-   $intNewInvNo = 0;
-   $intNewRefNo = 'NULL';
-   if ($addInvoiceNumber || $addReferenceNumber)
-   {
-     $strQuery = "SELECT max(invoice_no) FROM ". _DB_PREFIX_. "_invoice";
-     $intRes = mysql_query($strQuery);
-     $intInvNo = mysql_result($intRes, 0, 0) + 1;
-     if ($addInvoiceNumber)
-       $intNewInvNo = $intInvNo;
-     if ($addReferenceNumber)
-       $intNewRefNo = $intInvNo . miscCalcCheckNo($intInvNo);
-   }
+    $intNewInvNo = 0;
+    $intNewRefNo = 'NULL';
+    if ($addInvoiceNumber || $addReferenceNumber)     
+    {
+      $strQuery = "SELECT max(invoice_no) FROM {prefix}invoice";
+      $intRes = mysql_query_check($strQuery);
+      $intInvNo = mysql_result($intRes, 0, 0) + 1;
+      if ($addInvoiceNumber)
+        $intNewInvNo = $intInvNo;
+      if ($addReferenceNumber)
+        $intNewRefNo = $intInvNo . miscCalcCheckNo($intInvNo);
+    }
     
     $intRefundedId = $boolRefund ? $intInvoiceId : 'NULL';
     $strQuery = 
-        "INSERT INTO ". _DB_PREFIX_. "_invoice(name, company_id, invoice_no, real_invoice_no, invoice_date, due_date, payment_date, ref_number, state_id, reference, base_id, refunded_invoice_id) ".
-        "VALUES('$strname', $intCompanyId, $intNewInvNo, 0, $intDate, $intDueDate, NULL, $intNewRefNo, 1, '$strReference', $intBaseId, $intRefundedId )";
-
-    $intRes = mysql_query_check($strQuery);
-    $intNewId = mysql_insert_id();
+        "INSERT INTO {prefix}invoice(name, company_id, invoice_no, real_invoice_no, invoice_date, due_date, payment_date, ref_number, state_id, reference, base_id, refunded_invoice_id) ".
+        "VALUES(?, ?, ?, 0, ?, ?, NULL, ?, 1, ?, ?, ?)";
         
+    mysql_param_query($strQuery, array($strname, $intCompanyId, $intNewInvNo, $intDate, $intDueDate, $intNewRefNo, $strReference, $intBaseId, $intRefundedId));
+    $intNewId = mysql_insert_id();
     if( $intNewId ) {    
         $strQuery = 
             "SELECT * ".
-            "FROM ". _DB_PREFIX_. "_invoice_row ".
-            "WHERE invoice_id = $intInvoiceId";
-        $intRes = mysql_query($strQuery);
-        $intNRows = mysql_numrows($intRes);
-        for( $i = 0; $i < $intNRows; $i++ ) {
-            $intProductId = mysql_result($intRes, $i, "product_id");
-            $strDescription = mysql_result($intRes, $i, "description");
-            $intTypeId = mysql_result($intRes, $i, "type_id");
-            $intPcs = mysql_result($intRes, $i, "pcs");
-            $intPrice = mysql_result($intRes, $i, "price");
-            $intRowDate = mysql_result($intRes, $i, "row_date");
-            $intVat = mysql_result($intRes, $i, "vat");
-            $intOrderNo = mysql_result($intRes, $i, "order_no");
-            $boolVatIncluded = mysql_result($intRes, $i, "vat_included");
+            "FROM {prefix}invoice_row ".
+            "WHERE invoice_id = ?";
+        $intRes = mysql_param_query($strQuery, array($intInvoiceId));
+        while ($row = mysql_fetch_assoc($intRes)) {
+            $intProductId = $row['product_id'];
+            $strDescription = $row['description'];
+            $intTypeId = $row['type_id'];
+            $intPcs = $row['pcs'];
+            $intPrice = $row['price'];
+            $intRowDate = $row['row_date'];
+            $intVat = $row['vat'];
+            $intOrderNo = $row['order_no'];
+            $boolVatIncluded = $row['vat_included'];
+            $intReminderRow = $row['reminder_row'];
 
             if ($boolRefund)
               $intPcs = -$intPcs;
+            else if ($intReminderRow)
+              continue;
             
             $strQuery = 
-                "INSERT INTO ". _DB_PREFIX_. "_invoice_row(invoice_id, product_id, description, type_id, pcs, price, row_date, vat, order_no, vat_included) ".
-                "VALUES($intNewId, $intProductId, '$strDescription', $intTypeId, $intPcs, $intPrice, $intRowDate, $intVat, $intOrderNo, $boolVatIncluded )";
-            $intRes = mysql_query_check($strQuery);
+                "INSERT INTO {prefix}invoice_row(invoice_id, product_id, description, type_id, pcs, price, row_date, vat, order_no, vat_included, reminder_row) ".
+                "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            $intRes = mysql_param_query($strQuery, array($intNewId, $intProductId, $strDescription, $intTypeId, $intPcs, $intPrice, $intRowDate, $intVat, $intOrderNo, $boolVatIncluded, $intReminderRow));
         }
     }
 }
