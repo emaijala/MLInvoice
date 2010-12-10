d223cff6ca1d2a89b607ca99ad6469e22ff2f196
emaijala <emaijala@893e5d92-dae6-4f76-a1ff-a647e29d1ad7>
2010-12-10 21:47:46 +0000

    Override TCPDF's getRandomSeed as it's very slow in XAMPP.
    git-svn-id: https://vllasku.svn.sourceforge.net/svnroot/vllasku/trunk@73 893e5d92-dae6-4f76-a1ff-a647e29d1ad7

--- pdf.php
+++ pdf.php
@@ -22,5 +22,41 @@ class PDF extends TCPDF
     $this->SetX(150);
     $this->Cell(50, 5, $this->footerRight, 0, 0, "R");
   }
+
+  // Disable openssl_random_pseudo_bytes call as it's very slow on Windows  
+  protected function getRandomSeed($seed='') {
+    $seed .= microtime();
+    //if (function_exists('openssl_random_pseudo_bytes')) {
+    //  $seed .= openssl_random_pseudo_bytes(512);
+    //}
+    $seed .= uniqid('', true);
+    $seed .= rand();
+    $seed .= getmypid();
+    $seed .= __FILE__;
+    $seed .= $this->bufferlen;
+    if (isset($_SERVER['REMOTE_ADDR'])) {
+      $seed .= $_SERVER['REMOTE_ADDR'];
+    }
+    if (isset($_SERVER['HTTP_USER_AGENT'])) {
+      $seed .= $_SERVER['HTTP_USER_AGENT'];
+    }
+    if (isset($_SERVER['HTTP_ACCEPT'])) {
+      $seed .= $_SERVER['HTTP_ACCEPT'];
+    }
+    if (isset($_SERVER['HTTP_ACCEPT_ENCODING'])) {
+      $seed .= $_SERVER['HTTP_ACCEPT_ENCODING'];
+    }
+    if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+      $seed .= $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+    }
+    if (isset($_SERVER['HTTP_ACCEPT_CHARSET'])) {
+      $seed .= $_SERVER['HTTP_ACCEPT_CHARSET'];
+    }
+    $seed .= rand();
+    $seed .= uniqid('', true);
+    $seed .= microtime();
+    return $seed;
+  }
+  
 } 
 ?>
