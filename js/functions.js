067df7a75913b9b542b9151e070e1787bd0ffb85
emaijala <emaijala@893e5d92-dae6-4f76-a1ff-a647e29d1ad7>
2011-01-12 19:09:03 +0000

    Fixed sort by date in lists.
    git-svn-id: https://vllasku.svn.sourceforge.net/svnroot/vllasku/trunk@143 893e5d92-dae6-4f76-a1ff-a647e29d1ad7

--- js/functions.js
+++ js/functions.js
@@ -2,21 +2,21 @@ function sort_multi(a,b)
 {
   a = a.replace( /<.*?>/g, "" );
   b = b.replace( /<.*?>/g, "" );
-  var float_re = /^\d+\.?\d*$/;
   var date_re = /^(\d{1,2})\.(\d{1,2})\.(\d{4})$/;
+  var am = a.match(date_re);
+  var bm = b.match(date_re);
+  if (am && bm)
+  {
+    ad = am[3] + '.' + am[2] + '.' + am[1];
+    bd = bm[3] + '.' + bm[2] + '.' + bm[1];
+    return ((ad < bd) ? -1 : ((ad > bd) ?  1 : 0));
+  }
+  var float_re = /^\d+\.?\d*$/;
   if (a.match(float_re) && b.match(float_re))
   {
     a = parseFloat(a);
     b = parseFloat(b);
     return ((a < b) ? -1 : ((a > b) ?  1 : 0));
-  }
-  var am = a.match(date_re);
-  var bm = b.match(date_re);
-  if (am && bm)
-  {
-    ad = am[2] + '.' + am[1] + '.' + am[0];
-    bd = bm[2] + '.' + bm[1] + '.' + bm[0];
-    return ((ad < bd) ? -1 : ((ad > bd) ?  1 : 0));
   }
   a = a.toLowerCase();
   b = b.toLowerCase();
