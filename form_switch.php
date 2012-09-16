f0346825c318deca04e1e14ff314b99b79f7c167
Ere Maijala <ere@labs.fi>
2012-09-16 11:13:30 +0300

    Fixed manual update button of invoice number and reference number.

--- form_switch.php
+++ form_switch.php
@@ -204,7 +204,7 @@ onchange = "$.getJSON('json.php?func=get_company', {id: $('#company_id').val() }
 EOS;
 
     $getInvoiceNr = <<<EOS
-$.getJSON('json.php?func=get_invoice_defaults, {id: $('#record_id').val(), base_id: $('base_id').val(), interval_type: $('#interval_type').val()}, function(json) { $('#invoice_no').val(json.invoice_no); $('#ref_number').val(json.ref_no); $('.save_button').addClass('ui-state-highlight'); }); return false;
+$.getJSON('json.php?func=get_invoice_defaults', {id: $('#record_id').val(), base_id: $('base_id').val(), interval_type: $('#interval_type').val()}, function(json) { $('#invoice_no').val(json.invoice_no); $('#ref_number').val(json.ref_no); $('.save_button').addClass('ui-state-highlight'); }); return false;
 EOS;
 
     $locUpdateDates = $GLOBALS['locUpdateDates'];
