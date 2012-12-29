ed4e35617ae9d8fbe04ab9beb68f4ec313bf17ec
Ere Maijala <ere@labs.fi>
2012-12-29 21:52:26 +0200

    Fixed a problem with v1.5 database update script that caused prices equal to or more than 10000 to be truncated.

--- update_database_1.4_to_1.5.sql
+++ update_database_1.4_to_1.5.sql
@@ -1,6 +1,6 @@
-ALTER TABLE vllasku_product CHANGE COLUMN unit_price unit_price decimal(9,5);
+ALTER TABLE vllasku_product CHANGE COLUMN unit_price unit_price decimal(15,5);
 
-ALTER TABLE vllasku_invoice_row CHANGE COLUMN price price decimal(9,5);
+ALTER TABLE vllasku_invoice_row CHANGE COLUMN price price decimal(15,5);
 
 INSERT INTO vllasku_print_template (name, filename, parameters, output_filename, type, order_no, inactive) VALUES ('Lomakkeeton lasku', 'invoice_printer_formless.php', 'invoice,fi,N', 'lasku_%d.pdf', 'invoice', 70, 1);
 
@@ -8,6 +8,6 @@ ALTER TABLE vllasku_product ADD COLUMN (
   price_decimals decimal(1,0) NOT NULL default 2
 );
 
-ALTER TABLE vllasku_product CHANGE COLUMN discount discount decimal(3,1) NULL;
+ALTER TABLE vllasku_product CHANGE COLUMN discount discount decimal(4,1) NULL;
 
-ALTER TABLE vllasku_invoice_row CHANGE COLUMN discount discount decimal(3,1) NULL;
+ALTER TABLE vllasku_invoice_row CHANGE COLUMN discount discount decimal(4,1) NULL;
