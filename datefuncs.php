be5ba5867a34993998706ced8ae75b84d1c31f46
Ere Maijala <ere@labs.fi>
2014-02-02 21:24:06 +0200

    Don't save invalid dates to database.

--- datefuncs.php
+++ datefuncs.php
@@ -16,7 +16,7 @@ Tämä ohjelma on vapaa. Lue oheinen LICENSE.
 *******************************************************************************/
 
 // Convert database format to user-readable
-function dateConvDBDate2Date($intDate, $format = '') 
+function dateConvDBDate2Date($intDate, $format = '')
 {
   if (!$intDate)
     return '';
@@ -27,17 +27,20 @@ function dateConvDBDate2Date($intDate, $format = '')
 }
 
 // Convert user-readable format to database
-function dateConvDate2DBDate($strDate) 
+function dateConvDate2DBDate($strDate)
 {
   $arr = date_parse_from_format($GLOBALS['locDateFormat'], $strDate);
+  if ($arr['error_count'] > 0) {
+    return false;
+  }
   return sprintf('%04d%02d%02d', $arr['year'], $arr['month'], $arr['day']);
 }
 
 // Convert the user-readable date to unix time
-function strDate2UnixTime($strDate) 
+function strDate2UnixTime($strDate)
 {
   $arr = date_parse_from_format($GLOBALS['locDateFormat'], $strDate);
-  return mktime(0, 0, 0, $arr['month'], $arr['day'], $arr['year']); 
+  return mktime(0, 0, 0, $arr['month'], $arr['day'], $arr['year']);
 }
 
 // Convert the database format to unix time
