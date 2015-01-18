c0f1c6842cfd2a7d285ec5ad0e59fed383905fd3
Ere Maijala <ere@labs.fi>
2015-01-18 19:27:02 +0200

    Added stock_balance.js missed from previous commits.

---
+++ js/stock_balance.js
@@ -0,0 +1,66 @@
+function update_stock_balance(translations)
+{
+  var buttons = new Object();   
+  buttons[translations["save"]] = function() { save_stock_balance(translations); };
+  buttons[translations["close"]] = function() { $("#update_stock_balance").dialog("close"); };
+  $("#update_stock_balance").dialog({ modal: true, width: 400, height: 220, resizable: false, zIndex: 900,
+    buttons: buttons,
+    title: translations["title"],
+  });  
+}
+
+function save_stock_balance(translations)
+{
+  $.ajax({
+    "url": "json.php?func=update_stock_balance",
+    "type": "POST",
+    "data": {
+      "product_id": $("#record_id").val(),
+      "stock_balance_change": document.getElementById("stock_balance_change").value.replace(translations["decimal_separator"], '.'),
+      "stock_balance_change_desc": document.getElementById("stock_balance_change_desc").value
+    },
+    "success": function(data) {
+      if (data.missing_fields)
+      {
+        alert(translations["missing"] + data.missing_fields);
+      }
+      else
+      {
+        var new_balance = parseFloat(data.new_stock_balance).toFixed(2).replace('.', translations["decimal_separator"]);
+        $("#stock_balance").val(new_balance);
+        update_stock_balance_log();
+        $("#update_stock_balance").dialog("close");
+      }
+    },
+    "error": function(XMLHTTPReq, textStatus, errorThrown) {
+      if (textStatus == "timeout")
+        alert("Timeout trying to save stock balance change");
+      else
+        alert("Error trying to save stock balance change: " + XMLHTTPReq.status + " - " + XMLHTTPReq.statusText);
+      return false;
+    }
+  });    
+}
+
+function update_stock_balance_log()
+{
+  $("#stock_balance_change_log > tr").slice(0).remove();
+  $.ajax({
+    "url": "json.php?func=get_stock_balance_rows",
+    "type": "POST",
+    "data": {
+      "product_id": $("#record_id").val(),
+    },
+    "success": function(data) {
+      $("#stock_balance_change_log").append(data);
+    },
+    "error": function(XMLHTTPReq, textStatus, errorThrown) {
+      if (textStatus == "timeout")
+        alert("Timeout trying to save stock balance change");
+      else
+        alert("Error trying to save stock balance change: " + XMLHTTPReq.status + " - " + XMLHTTPReq.statusText);
+      return false;
+    }
+  });
+}
+
