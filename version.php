5f5faec280fa64550949226bf5d23226e11dcf4c
Ere Maijala <ere@labs.fi>
2015-10-31 16:18:14 +0200

    Updated version number to 1.12.0.

--- version.php
+++ version.php
@@ -2,16 +2,16 @@
 /*******************************************************************************
  MLInvoice: web-based invoicing application.
  Copyright (C) 2010-2015 Ere Maijala
- 
+
  This program is free software. See attached LICENSE.
- 
+
  *******************************************************************************/
 
 /*******************************************************************************
  MLInvoice: web-pohjainen laskutusohjelma.
  Copyright (C) 2010-2015 Ere Maijala
- 
+
  Tämä ohjelma on vapaa. Lue oheinen LICENSE.
- 
+
  *******************************************************************************/
-$softwareVersion = '1.11.1';
+$softwareVersion = '1.12.0';
