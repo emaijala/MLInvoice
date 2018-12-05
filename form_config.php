b31a330586f534f2fe2c00894de24f8c656f4282
Ere Maijala <ere@labs.fi>
2018-12-05 19:09:44 +0200

    Fix copyright year.

--- form_config.php
+++ form_config.php
@@ -4,7 +4,7 @@
  *
  * PHP version 5
  *
- * Copyright (C) 2010-2018 Ere Maijala
+ * Copyright (C) 2018 Ere Maijala
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
