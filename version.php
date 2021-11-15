b442ba12b986cc4ecabe3f74acec76388d1b1d06
Ere Maijala <ere@labs.fi>
2021-11-15 22:16:37 +0200

    Bump version to 2.0.0b3.

--- version.php
+++ version.php
@@ -25,4 +25,4 @@
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     http://labs.fi/mlinvoice.eng.php
  */
-$softwareVersion = '2.0.0b2';
+$softwareVersion = '2.0.0b3';
