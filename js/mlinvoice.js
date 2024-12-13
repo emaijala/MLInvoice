8aa612f99adb9a794556fe3eb9ae7869623f387f
Ere Maijala <ere@labs.fi>
2024-12-13 22:20:08 +0200

    Fix toast messages to use correct colors in dark mode too.

--- js/mlinvoice.js
+++ js/mlinvoice.js
@@ -305,7 +305,7 @@ var MLInvoice = (function CreateMLInvoice() {
     if (typeof colorClasses !== 'undefined') {
       $toast.addClass(colorClasses);
     } else {
-      $toast.addClass('text-white bg-success');
+      $toast.addClass('text-success-emphasis bg-success-subtle');
     }
     var $flex = $('<div class="d-flex">')
       .appendTo($toast);
@@ -334,7 +334,12 @@ var MLInvoice = (function CreateMLInvoice() {
 
   function errormsg(msg, timeout)
   {
-    infomsg(msg, timeout, 'text-white bg-danger');
+    infomsg(msg, timeout, 'text-danger-emphasis bg-danger-subtle');
+  }
+
+  function warningmsg(msg, timeout)
+  {
+    infomsg(msg, timeout, 'text-warning-emphasis bg-warning-subtle');
   }
 
   function clearMessages()
@@ -803,6 +808,7 @@ var MLInvoice = (function CreateMLInvoice() {
     updateRowSelectedState: updateRowSelectedState,
     infomsg: infomsg,
     errormsg: errormsg,
+    warningmsg: warningmsg,
     editUnitPrice: editUnitPrice,
     setCurrencyDecimals: setCurrencyDecimals,
     getDateFormat: getDateFormat,
