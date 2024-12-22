3dcb4d21db073c4ea1fef0a2200bac6f0da005cc
Ere Maijala <ere@labs.fi>
2024-12-22 23:20:38 +0200

    Fix "search in middle" setting being backwards in lists.
    Fixes the negated setting as reported in #418, but needs further consideration.

--- list.php
+++ list.php
@@ -813,7 +813,7 @@ function createListQuery($strFunc, $strList, $startRow, $rowCount, $sort,
 
     $filteredQb = clone $qb;
     if ($filter) {
-        $leftAnchored = getSetting('dynamic_select_search_in_middle');
+        $leftAnchored = !getSetting('dynamic_select_search_in_middle');
         $termPrefix = $leftAnchored ? '' : '%';
         foreach (explode(' ', $filter) as $term) {
             if ('' === trim($term)) {
